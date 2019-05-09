d323c57b838ea2267d61c022b856e0691978f115
may54ther <aha9654@gmail.com>
2019-05-10 01:13:21 +0900

    design: CSS 수정

--- src/store.js
+++ src/store.js
@@ -7,13 +7,16 @@ export default new Vuex.Store({
   state: {
     apikey: "50d58edebb16d9143557fc5550d5d78b",
     url: {
-      TMDb: "https://api.themoviedb.org",
+      TMDb: "https://api.themoviedb.org/3",
       lang_ko: "&language=ko-KO",
+      sort_by: "&sort_by=papularity.desc",
       discover: "/discover/movie?",
       upcoming: "/movie/upcoming?",
       search: "/search/movie?"
+      // credits: `/movie/movie-id/credits?`
     }
   },
+  getters: {},
   mutations: {},
   actions: {}
 });
