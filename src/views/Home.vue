28b1f849463b5adc1f24edf6faee892f82f02694
may54ther <aha9654@gmail.com>
2019-05-10 01:13:21 +0900

    update css/setting code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="container">
     <div class="slider">
+      <h2 class="blind">Hot Movies</h2>
       <ul class="slider__items">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li>
+          <div class="item__img"></div>
+          <div class="itme__txt">
+            <h3 class="txt__title">어벤져스: 엔드게임</h3>
+            <p class="txt__subject">Avengers: End Game, 2019-04-24</p>
+            <p class="txt__desc">인피니티 워 이후 절반만 살아남은 지구, 마지막 희망이 된 어벤져스! 먼저 떠넌 그들을 위해 모든 것을 걸었다.</p>
+          </div>
+        </li>
       </ul>
       <ul class="slider__buttons">
         <li></li>
@@ -27,6 +31,9 @@
 
 <script>
 export default {
-  name: "home"
+  name: "home",
+  data() {
+    return {};
+  }
 };
 </script>
