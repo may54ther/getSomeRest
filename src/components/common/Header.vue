c8afd2e65560e5f7ddcc1a8d75d39a0f5bae1ba1
may54ther <aha9654@gmail.com>
2019-05-15 03:08:05 +0900

    move file: header/footer를 common 폴더로 이동

---
+++ src/components/common/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="header">
+    <h1 class="logo">
+      <router-link :to="{'name':'home'}">
+        <span class="blind">getSomeRest</span>
+      </router-link>
+    </h1>
+    <span class="button__search">
+      <router-link :to="{'name':'search'}">
+        <i class="fas fa-search"></i>
+      </router-link>
+    </span>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
