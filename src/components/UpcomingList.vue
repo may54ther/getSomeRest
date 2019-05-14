bb69d550a3d6d8d4e808aefab9934093c1569bd2
may54ther <aha9654@gmail.com>
2019-05-15 03:10:51 +0900

    feat: 상영중인 영화 리스트 불러오기

---
+++ src/components/UpcomingList.vue
@@ -0,0 +1,65 @@
+<template>
+  <section class="content__upcoming">
+    <h2>현재 상영중인 영화</h2>
+    <ul>
+      <li v-bind:key="upcoming[0]" v-for="upcoming in upcomingDatas">
+        <a>
+          <div class="upcoming__thumbnail">
+            <img :src="upcoming[1]" alt="포스터">
+          </div>
+          <p class="upcoming__title">{{upcoming[2]}}</p>
+          <span class="upcoming__release">{{upcoming[3]}}</span>
+        </a>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  name: "UpcomingList",
+  props: [],
+  data() {
+    return {
+      upcomingDatas: []
+    };
+  },
+  methods: {
+    getUpcomingList() {
+      this.axios
+        .get(this.url.TMDb + this.url.upcoming, {
+          params: {
+            api_key: this.params.apiKey,
+            language: this.params.langKo,
+            page: 1
+          }
+        })
+        .then(res => {
+          const result = res.data.results;
+
+          result.forEach((data, idx) => {
+            if (idx >= 10) return;
+
+            this.upcomingDatas.push([
+              data.id,
+              this.imgUrl.poster + data.poster_path,
+              data.title,
+              data.release_date
+            ]);
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        })
+        .finally(() => {});
+    }
+  },
+  computed: {
+    ...mapState(["url", "params", "imgUrl"])
+  },
+  created() {
+    this.getUpcomingList();
+  }
+};
+</script>
