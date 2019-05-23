af0440e45dd3adcf0fa6c12ceaf998ce83ae51e4
may54ther <aha9654@gmail.com>
2019-05-24 03:55:17 +0900

    fix, feat: 페이지 이동 오류 수정, 슬라이더 트랜지션 추가

--- src/components/information/Movie.vue
+++ src/components/information/Movie.vue
@@ -18,7 +18,7 @@
 
           <dd class="blind">장르, 개봉일, 상영시간, 평점</dd>
           <dt class="desc2">
-            <span class="txt__genres">{{genres}}</span>
+            <span class="txt__genres">{{ movieGenres }}</span>
             <span class="txt__release-date">{{movieData.release_date}}</span>
             <span class="txt__runtime">{{movieData.runtime}}분</span>
             <span class="txt__vote-avg">🔥 {{movieData.vote_average}}/10</span>
@@ -32,79 +32,9 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
-
 export default {
   name: "Movies",
-  props: ["movieId"],
-  data() {
-    return {
-      movieData: {},
-      genres: null
-    };
-  },
-  computed: {
-    ...mapState(["url", "params", "imgURL"])
-  },
-  methods: {
-    overviewLength() {
-      let ww = window.width;
-      console.log(ww);
-    },
-    getMovieData(id) {
-      this.axios
-        .get(this.url.TMDb + `/movie/${id}`, {
-          params: {
-            api_key: this.params.apiKey,
-            language: this.params.langKo
-          }
-        })
-        .then(res => {
-          const result = res.data;
-
-          this.movieData = {
-            backdrop_path:
-              result.backdrop_path === null
-                ? null
-                : this.imgURL.backdrop + result.backdrop_path,
-            poster_path:
-              result.poster_path === null
-                ? null
-                : this.imgURL.poster + result.poster_path,
-            title: result.title,
-            original_title: result.original_title,
-            release_date: result.release_date,
-            genres: result.genres,
-            runtime: result.runtime,
-            vote_average: result.vote_average,
-            tagline: result.tagline,
-            overview: (result.overview =
-              result.overview.length > 300
-                ? result.overview.substr(0, 300) + "..."
-                : result.overview)
-          };
-        })
-        .catch(err => {
-          console.log(err);
-        })
-        .finally(() => {
-          const temp = [];
-          this.movieData.genres.forEach(data => {
-            temp.push(data.name);
-          });
-          this.genres = temp.toString().replace(/,/g, ", ");
-
-          document.querySelector(
-            ".sec__movie"
-          ).style.background = `linear-gradient(rgba(0,0,0,.8), rgba(0,0,0,.8)), url(${
-            this.movieData.backdrop_path
-          }) center / cover no-repeat`;
-        });
-    }
-  },
-  created() {
-    this.getMovieData(this.movieId);
-  }
+  props: ["movieData", "movieGenres"]
 };
 </script>
 
--- src/components/information/SimilarMovies.vue
+++ src/components/information/SimilarMovies.vue
@@ -2,8 +2,8 @@
   <section class="sec__similar" v-if="similarMovies.length !== 0">
     <h2>유사 영화 소개</h2>
     <ul class="inner clearfix">
-      <li v-bind:key="index" v-for="(item, index) in similarMovies">
-        <a @click="$store.commit('routerMovieInfo', item.id)">
+      <li v-bind:key="index" v-for="(item, index) in this.similarMovies">
+        <a @click="getNewMovieId(item.id)">
           <div class="movie-img">
             <img :src="item.poster" alt="포스터" v-if="item.poster !== null">
             <img src="../../assets/img_no_poster.png" alt="No-Data" v-if="item.poster === null">
@@ -16,55 +16,14 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
-
 export default {
   name: "SimilarMovies",
-  props: ["movieId"],
+  props: ["similarMovies", "getNewMovieId"],
   data() {
     return {
-      similarLength: 0,
-      similarMovies: []
+      similarLength: this.similarMovies.length
     };
-  },
-  computed: {
-    ...mapState(["url", "params", "imgURL"])
-  },
-  methods: {
-    getSimilarMovies(id) {
-      this.axios
-        .get(this.url.TMDb + `/movie/${id}/similar`, {
-          params: {
-            api_key: this.params.apiKey,
-            language: this.params.langKo
-          }
-        })
-        .then(res => {
-          const result = res.data.results;
-
-          result.forEach((data, idx) => {
-            if (data.poster_path !== "") {
-              this.similarMovies.push({
-                id: data.id,
-                title: data.title,
-                poster:
-                  data.poster_path === null
-                    ? null
-                    : this.imgURL.poster + data.poster_path
-              });
-            }
-          });
-        })
-        .catch(err => {
-          console.log(err);
-        })
-        .finally(() => {});
-    }
-  },
-  created() {
-    this.getSimilarMovies(this.movieId);
-  },
-  mounted() {}
+  }
 };
 </script>
 
