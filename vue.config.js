324abf3b4ea99c3578e862ed154d7938664cc00b
may54ther <aha9654@gmail.com>
2020-07-07 14:32:54 +0900

    delploy project

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ? 'https://may54ther.github.io/getSomeRest/dist/' : '/'
+	publicPath: process.env.NODE_ENV === 'production' ? '/dist/' : '/'
 }
